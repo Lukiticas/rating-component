e45a93003d2850808cb571197e821e292daf937a
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-08-25 15:34:10 -0300

    fixing some bugs - animation and html

--- js/index.js
+++ js/index.js
@@ -1,33 +1,52 @@
-const $ = (el) => document.querySelectorAll(el);
-const radios = [...$("input[name='rating']")];
-const ratingBody = $(".rating-body");
+const $ = (el) => [...document.querySelectorAll(el)];
 
-/* ANIMATION IN CSS BECAUSE WHY NOT */
-const shake = [
-  { transform: "translate3d(-1px, 0, 0)" },
-  { transform: "translate3d(2px, 0, 0)" },
-  { transform: "translate3d(-4px, 0, 0)" },
-  { transform: "translate3d(4px, 0, 0)" },
-  { transform: "translate3d(-4px, 0, 0)" },
-  { transform: "translate3d(4px, 0, 0)" },
-  { transform: "translate3d(-4px, 0, 0)" },
-  { transform: "translate3d(2px, 0, 0)" },
-  { transform: "translate3d(-1px, 0, 0)" },
-];
+const radios = $("input[name='rating']");
+const ratingBody = $(".rating-body");
+const labels = $(".rate");
+
+/* ANIMATIONS! */
+
+const invalidAnimate = (parent, labels) => {
+  /* ANIMATION IN CSS BECAUSE WHY NOT */
+  const shake = [
+    { transform: "translate3d(-1px, 0, 0)" },
+    { transform: "translate3d(2px, 0, 0)" },
+    { transform: "translate3d(-4px, 0, 0)" },
+    { transform: "translate3d(4px, 0, 0)" },
+    { transform: "translate3d(-4px, 0, 0)" },
+    { transform: "translate3d(4px, 0, 0)" },
+    { transform: "translate3d(-4px, 0, 0)" },
+    { transform: "translate3d(2px, 0, 0)" },
+    { transform: "translate3d(-1px, 0, 0)" },
+  ];
+
+  parent.animate(shake, {
+    duration: 820,
+    easing: "cubic-bezier(.36,.07,.19,.97)",
+    fill: "both",
+  });
+
+  labels.forEach((label) => {
+    label.classList.toggle("invalid");
+    setTimeout(() => {
+      label.classList.toggle("invalid");
+    }, 820);
+  });
+};
 
 /* SLIDE BETWEEN WINDOWS */
-const toggleWindows = (rate) => {
+const toggleWindows = (rateNumber) => {
   ratingBody.forEach((window) => {
-    $(".rate--submited")[0].innerText = rate;
+    $(".rate--submited")[0].innerText = rateNumber;
     window.classList.toggle("hidden");
   });
 };
 
 /* MAKES ALL CHECKBOXES BEHAVE LIKE RADIO FOR UNSELECTION */
 
-const toBehaveLikeRadio = (button) => {
+const toBehaveLikeRadio = (input) => {
   radios.forEach((item) => {
-    if (item !== button) item.checked = false;
+    if (item !== input) item.checked = false;
   });
 };
 
@@ -46,11 +65,7 @@ $(".rating-body__button").forEach((button) => {
       const givenRate = radios.filter((radio) => radio.checked == true)[0].id;
       toggleWindows(givenRate);
     } else {
-      ratingBody[0].animate(shake, {
-        duration: 820,
-        easing: "cubic-bezier(.36,.07,.19,.97)",
-        fill: "both",
-      });
+      invalidAnimate(ratingBody[0], labels);
     }
   });
 });
